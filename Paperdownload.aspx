dd2657bae685bce187acc3b7d42671b292490688
venugopal <88709360+venupathange@users.noreply.github.com>
2024-12-19 11:32:31 +0530

    Add files via upload

---
+++ Paperdownload.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Site.master" AutoEventWireup="true"  
+   EnableEventValidation="false" CodeFile="Paperdownload.aspx.cs" Inherits="Reports_Paperdownload"  
+   ValidateRequest="false" %>    
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="Server">
+    <TheoremWC:ToolBar ID="toolBar" runat="server" PageTitle="Configure Online Payment Paid Details">
+        <asp:UpdatePanel ID="update1" runat="server" UpdateMode="Conditional">
+            <ContentTemplate>
+                <table width="98%" style="margin: 0px 0px 0px 0px" border="0">
+                    <tr class="btn">
+                    <td align="left">
+                          <asp:LinkButton ID="btnPaymentDetails" OnClick="btnReport_Click" ValidationGroup="Report"  
+                              runat="server" Text="<img id='img' style='vertical-align:middle' src='../Images/pdf.gif' /> Report"  
+                 Style="vertical-align: middle; padding: 8px 16px; background-color: #4CAF50; color: white; border: none; border-radius: 4px; cursor: pointer;">
+                           </asp:LinkButton>  
+                    
+                      </td>
+                    </tr>
+                  </table>
+               </ContentTemplate>
+               <Triggers>
+            <asp:PostBackTrigger ControlID="btnPaymentDetails" />
+             <%--<asp:PostBackTrigger ControlID="btnPaidChallan" />--%>
+            </Triggers>
+            </asp:UpdatePanel>
+          </TheoremWC:ToolBar> 
+         <TheoremWC:PageContent ID="pc" runat="server">
+        <table width="100%">
+            <tr>  
+           <td width="16%" align="left">  
+              <label for="txtPaymentDate"><b>Date</b></label><br />  
+              <asp:TextBox ID="txtPaymentDate" OnBlur="OnBlur(this);" OnFocus="OnFocus(this);"  
+                Font-Size="Small" Width="150px" runat="server" placeholder="--ALL--" class="date-input">  
+              </asp:TextBox>  
+              <ajaxToolkit:CalendarExtender ID="cePaymentDate" runat="server"  
+                TargetControlID="txtPaymentDate" EnabledOnClient="true" Format="dd/MM/yyyy" />  
+              <br />  
+              <ajaxToolkit:FilteredTextBoxExtender ID="FilteredTextBoxExtender1" runat="server"  
+                TargetControlID="txtPaymentDate" FilterType="Numbers, Custom" ValidChars="/" />  
+           </td>
+           
+               <td align="left" width="30%">
+                    <b>Session</b>
+                    <br />
+                    <asp:DropDownList ID="ddlSession" runat="server" DataTextField="Name" DataValueField="Id" Width="60%">
+                    <asp:ListItem Text="Morning" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="AfterNoon" Value="2"></asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+           </tr>
+            </table>  
+            <Trigger>
+            <asp:AsyncPostBackTrigger ControlID="LinkButton1" EventName="Click" /> 
+            </Trigger>
+      </TheoremWC:PageContent>
+</asp:Content> 
+          
+    
+ 
---
+++ Paperdownload.aspx.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using Theorem.EOrg.Core.BAL;
+using Theorem.EOrg.Core.DEO;
+using Theorem.EOrg.StudentCore.BAL;
+using Theorem.EOrg.StudentCore.DEO;
+using Theorem.EOrg.Framework.Utilities;
+using DbLinq.Factory;
+using System.Data;
+using CrystalDecisions.Web;
+using CrystalDecisions.Shared;
+using CrystalDecisions.Enterprise;
+using CrystalDecisions.CrystalReports;
+using CrystalDecisions.CrystalReports.Engine;
+using BarCode;
+using System.IO;
+using System.Web.Services;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Services;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using OfficeOpenXml.Utils;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.ConditionalFormatting;
+using OfficeOpenXml.VBA;
+using OfficeOpenXml.Table;
+using System.Drawing;  
+using System.Web.Security;
+public partial class Reports_Paperdownload : PageBase
+{
+
+    UserManager objUserManager = null;
+    AdminManager objAdminManager = null;
+    StudentManager _studentMgr = null;
+    DataSet ds = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+    }
+
+    protected void btnReport_Click(object sender, EventArgs e)
+    {
+        AssignValues_Excel();
+    }
+    private void AssignValues_Excel()
+    {
+
+        try
+        {
+            CreateStudentManagerInstance();
+            string Date = this.txtPaymentDate.Text == "" ? "-1" : this.txtPaymentDate.Text;
+
+
+            DataSet ds = _studentMgr.Getquestionpaperdownload(Date, Convert.ToInt32(this.ddlSession.SelectedValue));
+            DataSet ds=_studentMgr.
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                //Create the worksheet
+
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Download");
+
+
+                ws.Cells["A1"].LoadFromDataTable(ds.Tables[0], true);
+
+
+                int columncount = ds.Tables[0].Columns.Count;
+                //string strcolumnsize;
+
+
+                //Format the header for column 1-3
+                using (ExcelRange rng = ws.Cells["A1:D1"])
+                {
+                    rng.Style.Font.Bold = true;
+                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;                      //Set Pattern for the background to Solid
+                    rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));  //Set color to dark blue
+                    rng.Style.Font.Color.SetColor(Color.White);
+                }
+
+                //Example how to Format Column 1 as numeric 
+                using (ExcelRange col = ws.Cells[2, 1, 2 + ds.Tables[0].Rows.Count + 1, 1])
+                {
+                    //col.Style.Numberformat.Format = "#,##0.00";
+                    col.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                }
+
+                for (int i = 0; i <= ds.Tables[0].Columns.Count; i++)
+                {
+                    //ws.Column(i).Width = 30;
+                    ws.Column(i + 1).AutoFit();
+                    ws.Column(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    ws.Column(4).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    ws.Column(6).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                }
+
+
+                //Write it back to the client
+                string ExlFile = System.IO.Path.GetTempPath() + "Demoaa.xlsx";
+                FileInfo fInfo = new FileInfo(ExlFile);
+
+                // pck.SaveAs(fInfo);
+                Response.Write(pck.GetAsByteArray());
+
+                //  Response.BinaryWrite(pck.GetAsByteArray());
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                //Response.ContentType = "application/vnd.ms-excel.12";
+                // Response.AddHeader("content-disposition", "attachment;  filename=OnlinePaymentPaidDetails" + DateTime.Now.ToString("dd/MM/yyyy") + ".xlsx");
+                Response.AddHeader("content-disposition", "attachment; filename=OnlinePaymentPaidDetails_" + DateTime.Now.ToString("yyyyMMdd") + ".xlsx");
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            throw ex;
+        }
+        update1.Update();
+    }
+
+
+    private void CreateUserManagerInstance()
+    {
+        if (objUserManager == null)
+            objUserManager = new UserManager();
+    }
+    private void CreateStudentManagerInstance()
+    {
+        if (_studentMgr == null)
+            _studentMgr = new StudentManager();
+    }
+
+}
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
+//using System.Xml.Linq;
+//using Theorem.EOrg.Core.BAL;
+//using Theorem.EOrg.Core.DEO;
+//using Theorem.EOrg.StudentCore.BAL;
+//using Theorem.EOrg.StudentCore.DEO;
+//using Theorem.EOrg.Framework.Utilities;
+//using DbLinq.Factory;
+//using System.Data;
+//using CrystalDecisions.Web;
+//using CrystalDecisions.Shared;
+//using CrystalDecisions.Enterprise;
+//using CrystalDecisions.CrystalReports;
+//using CrystalDecisions.CrystalReports.Engine;
+//using BarCode;
+//using System.IO;
+//using System.Web.Services;
+//using System.Text.RegularExpressions;
+//using System.Configuration;
+//using System.Data.SqlClient;
+//using System.Web.Script.Services;
+//using OfficeOpenXml;
+//using OfficeOpenXml.Style;
+//using OfficeOpenXml.Utils;
+//using OfficeOpenXml.Drawing;
+//using OfficeOpenXml.ConditionalFormatting;
+//using OfficeOpenXml.VBA;
+//using OfficeOpenXml.Table;
+//using System.Drawing;
+//using System.Web.Security;
+//using System.Diagnostics;
+
+//public partial class Reports_Paperdownload : PageBase
+//{
+//    UserManager objUserManager = null;
+//    AdminManager objAdminManager = null;
+//    StudentManager _studentMgr = null;
+//    DataSet ds = null;
+
+//    protected void Page_Load(object sender, EventArgs e)
+//    {
+//        if (!IsPostBack)
+//        {
+
+//        }
+//    }
+
+//    protected void btnReport_Click(object sender, EventArgs e)
+//    {
+//        AssignValues_Excel();
+//    }
+
+//    private void AssignValues_Excel()
+//    {
+//        try
+//        {
+//            CreateStudentManagerInstance();
+//            string Date = this.txtPaymentDate.Text == "" ? "-1" : this.txtPaymentDate.Text;
+
+//            DataSet ds = _studentMgr.Getquestionpaperdownload(Date, Convert.ToInt32(this.ddlSession.SelectedValue));
+
+//            using (ExcelPackage pck = new ExcelPackage())
+//            {
+//                //Create the worksheet  
+//                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Download");
+
+//                ws.Cells["A1"].LoadFromDataTable(ds.Tables[0], true);
+
+//                int columncount = ds.Tables[0].Columns.Count;
+
+//                //Format the header for column 1-3  
+//                using (ExcelRange rng = ws.Cells["A1:D1"])
+//                {
+//                    rng.Style.Font.Bold = true;
+//                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;               //Set Pattern for the background to Solid  
+//                    rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));  //Set color to dark blue  
+//                    rng.Style.Font.Color.SetColor(Color.White);
+//                }
+
+//                //Example how to Format Column 1 as numeric  
+//                using (ExcelRange col = ws.Cells[2, 1, 2 + ds.Tables[0].Rows.Count + 1, 1])
+//                {
+//                    //col.Style.Numberformat.Format = "#,##0.00";  
+//                    col.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+//                }
+
+//                for (int i = 0; i <= ds.Tables[0].Columns.Count; i++)
+//                {
+//                    //ws.Column(i).Width = 30;  
+//                    ws.Column(i + 1).AutoFit();
+//                    ws.Column(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+//                    ws.Column(4).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+//                    ws.Column(6).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+//                }
+
+//                //Write it back to the client  
+//                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+//                Response.AddHeader("content-disposition", "attachment; filename=QuestionPaperReport" + DateTime.Now.ToString("yyyyMMdd") + ".xlsx");
+
+//                Debug.WriteLine("Before writing to response stream");
+//                Response.BinaryWrite(pck.GetAsByteArray());
+//                Debug.WriteLine("After writing to response stream");
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//            Debug.WriteLine("Error occurred: " + ex.Message);
+//            // Log the exception or provide a more user-friendly error message  
+//        }
+//        update1.Update();
+//    }
+
+//    private void CreateUserManagerInstance()
+//    {
+//        if (objUserManager == null)
+//            objUserManager = new UserManager();
+//    }
+
+//    private void CreateStudentManagerInstance()
+//    {
+//        if (_studentMgr == null)
+//            _studentMgr = new StudentManager();
+//    }
+//}
